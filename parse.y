735c3532c5c36b9aefcfab8b81e748b8a99bbb88
Trea19 <1242734867@qq.com>
2023-05-18 21:57:39 +0800

    add syntax_tree

--- parse.y
+++ parse.y
@@ -1,187 +1,248 @@
 %{
-#include <stdio.h>
-#include <stdlib.h>
-int yylex(void);
-void yyerror(const char*);
-#define YYSTYPE char *
+#include<unistd.h>
+#include<stdio.h>   
+#include "syntax_tree.h"
 %}
 
-%token T_Int T_Void T_Return T_Print T_ReadInt T_While
-%token T_If T_Else T_Break T_Continue T_Le T_Ge T_Eq T_Ne
-%token T_And T_Or T_IntConstant T_StringConstant T_Identifier
+%union{
+    tnode type_tnode;
+	double d; // 这里声明double是为了防止出现指针错误-segmentation fault
+}
 
-%left '='
-%left T_Or
-%left T_And
-%left T_Eq T_Ne
-%left '<' '>' T_Le T_Ge
-%left '+' '-'
-%left '*' '/' '%'
-%left '!'
+/*声明记号*/
+%token <type_tnode> INT TYPE STRUCT RETURN IF ELSE WHILE BREAK ID COMMENT SPACE SEMI COMMA ASSIGNOP 
+%token <type_tnode> PLUS MINUS MULTI DIV AND OR DOT NOT LP RP LB RB LC RC AERROR RELOP EOL
 
+%type  <type_tnode> Program ExtDefList ExtDef ExtDecList Specifire StructSpecifire 
+%type  <type_tnode> OptTag Tag VarDec FunDec VarList ParamDec Compst StmtList Stmt DefList Def DecList Dec Exp Args
+
+/*优先级*/
+%right ASSIGNOP
+%left OR
+%left AND
+%left RELOP
+%left PLUS MINUS
+%left MULTI DIV
+%right NOT 
+%left LP RP LB RB DOT
+
+%nonassoc LOWER_THAN_ELSE 
+%nonassoc ELSE
+
+/*产生式*/
+/*$$表示左表达式 ${num}表示右边的第几个表达式*/
 %%
 
 Program:
-    /* empty */             { /* empty */ }
-|   Program FunctionDef        { /* empty */ }
+ExtDefList {$$=newAst("Program",1,$1); }
 ;
 
-FunctionDef:
-    ReturnType FuncName '(' Args ')' '{' VarDecls Statements '}'    {printf("ENDFUNC\n\n"); }
+ExtDefList:
+ExtDef ExtDefList {$$=newAst("ExtDefList",2,$1,$2); }
+| {$$=newAst("ExtDefList",0,-1); }
 ;
 
-ReturnType:
-    T_Int                   { /* empty */ }
-|   T_Void                  { /* empty */ }
+ExtDef:
+Specifire ExtDecList SEMI {
+	$$=newAst("ExtDef",3,$1,$2,$3);
+	if(findvar($2)) // 变量重定义
+		printf("Error at Line %d: Redefined Variable '%s'\n",yylineno,$2->content);
+    else newvar(2,$1,$2); //新增变量表符号 
+    }    
+| Specifire SEMI	{$$=newAst("ExtDef",2,$1,$2); }
+| Specifire FunDec Compst {
+		$$=newAst("ExtDef",3,$1,$2,$3); 
+		// 设置函数声明的返回值类型并检查返回类型错误
+		newfunc(1,$1);
+}
 ;
 
-FuncName:
-    T_Identifier            { printf("FUNC @%s:\n", $1);}
+ExtDecList:
+VarDec {$$=newAst("ExtDecList",1,$1); }
+|VarDec COMMA ExtDecList {$$=newAst("ExtDecList",3,$1,$2,$3); }
+;
+
+Specifire:
+TYPE {$$=newAst("Specifire",1,$1);}
+|StructSpecifire {$$=newAst("Specifire",1,$1); }
+;
+
+StructSpecifire:
+STRUCT OptTag LC DefList RC {
+		// 结构体定义完成，当前在结构体定义外部
+		inStruc = 0;
+		$$=newAst("StructSpecifire",5,$1,$2,$3,$4,$5); 
+		if(findstruc($2))	// 结构体的名字与前面定义过的结构体或变量的名字重复
+			printf("Error at Line %d: Redefined Struct Name '%s'\n",yylineno,$2->content);
+        else newstruc(1,$2); }
+|STRUCT Tag {$$=newAst("StructSpecifire",2,$1,$2); }
+;
+
+OptTag:
+ID {$$=newAst("OptTag",1,$1); }
+|{$$=newAst("OptTag",0,-1); }
+;
+
+Tag:
+ID {$$=newAst("Tag",1,$1); }
+;
+
+VarDec:
+ID {$$=newAst("VarDec",1,$1); $$->tag=1; //变量
+	}
+|VarDec LB INT RB { //数组
+	$$=newAst("VarDec",4,$1,$2,$3,$4); 
+	$$->content=$1->content;$$->tag=4;}
+;
+
+FunDec:
+ID LP VarList RP {
+		$$=newAst("FunDec",4,$1,$2,$3,$4); $$->content = $1->content;
+		if(findfunc($1)) //函数重定义
+			printf("Error at Line %d:Redefined Function '%s'\n",yylineno,$1->content);
+        else newfunc(2,$1,$3); // 设置函数名称以及参数列表 
+        }
+|ID LP RP {
+		$$=newAst("FunDec",3,$1,$2,$3); $$->content=$1->content;
+		if(findfunc($1)) //函数重定义
+			printf("Error at Line %d:Redefined Function '%s'\n",yylineno,$1->content);
+        else newfunc(2,$1,$3); 	// 设置函数名称以及参数列表 
+        }
+;
+
+VarList:
+ParamDec COMMA VarList {$$=newAst("VarList",3,$1,$2,$3); }
+|ParamDec {$$=newAst("VarList",1,$1); }
+;
+
+ParamDec:
+Specifire VarDec {
+		$$=newAst("ParamDec",2,$1,$2); 
+		if(findvar($2)||findarray($2))  // 变量重定义
+			printf("Error at Line %d:Redefined Variable '%s'\n",yylineno,$2->content);
+        else if($2->tag==4) //创建数组符号表
+			newarray(2,$1,$2);
+        else // 创建变量符号表
+			newvar(2,$1,$2); }
+;
+
+Compst:
+LC DefList StmtList RC {$$=newAst("Compst",4,$1,$2,$3,$4); }
+;
+
+StmtList:Stmt StmtList{$$=newAst("StmtList",2,$1,$2); }
+| {$$=newAst("StmtList",0,-1); }
+;
+
+Stmt:
+Exp SEMI {$$=newAst("Stmt",2,$1,$2); }
+|Compst {$$=newAst("Stmt",1,$1); }
+|RETURN Exp SEMI {
+		$$=newAst("Stmt",3,$1,$2,$3); 
+		getrtype($2);}
+|IF LP Exp RP Stmt %prec LOWER_THAN_ELSE {$$=newAst("Stmt",5,$1,$2,$3,$4,$5); }
+|IF LP Exp RP Stmt ELSE Stmt {$$=newAst("Stmt",7,$1,$2,$3,$4,$5,$6,$7); }
+|WHILE LP Exp RP Stmt {$$=newAst("Stmt",5,$1,$2,$3,$4,$5); }
+;
+
+/*Local Definitions*/
+DefList:
+Def DefList{$$=newAst("DefList",2,$1,$2); }
+| {$$=newAst("DefList",0,-1); }
+;
+
+Def:
+Specifire DecList SEMI {
+		$$=newAst("Def",3,$1,$2,$3); 
+		// 错误类型7:变量出现重复定义
+		if(findvar($2)||findarray($2))  
+			printf("Error type 7 at Line %d:Redefined Variable '%s'\n",yylineno,$2->content);
+        else if($2->tag==4) 
+			newarray(2,$1,$2);
+        else 
+			newvar(2,$1,$2);}
+;
+
+DecList:
+Dec {$$=newAst("DecList",1,$1); }
+|Dec COMMA DecList {$$=newAst("DecList",3,$1,$2,$3); $$->tag=$3->tag;}
+;
+
+Dec:
+VarDec {$$=newAst("Dec",1,$1); }
+|VarDec ASSIGNOP Exp {$$=newAst("Dec",3,$1,$2,$3); $$->content=$1->content;}
+;
+
+/*Expressions*/
+Exp:
+Exp ASSIGNOP Exp {
+		$$=newAst("Exp",3,$1,$2,$3); 
+		if($1->type==NULL || $3->type==NULL){ // 当有一边变量是未定义时，不进行处理
+			return;
+		}
+		if(strcmp($1->type,$3->type)) // 赋值号两边的表达式类型不匹配
+			printf("Error at Line %d:Type mismatched for assignment.\n ",yylineno);
+		if(!checkleft($1)) // 赋值号左边出现一个只有右值的表达式
+			printf("Error at Line %d:The left-hand side of an assignment must be a variable.\n ",yylineno); }
+|Exp AND Exp {$$=newAst("Exp",3,$1,$2,$3); }
+|Exp OR Exp {$$=newAst("Exp",3,$1,$2,$3); }
+|Exp RELOP Exp {$$=newAst("Exp",3,$1,$2,$3); }
+|Exp PLUS Exp {
+		$$=newAst("Exp",3,$1,$2,$3); 
+		// 操作数类型不匹配或操作数类型与操作符不匹配
+		if(strcmp($1->type,$3->type))
+			printf("Error at Line %d:Type mismatched for operands.\n ",yylineno);}
+|Exp MINUS Exp {
+		$$=newAst("Exp",3,$1,$2,$3); 
+		// 操作数类型不匹配或操作数类型与操作符不匹配
+		if(strcmp($1->type,$3->type))
+			printf("Error at Line %d:Type mismatched for operands.\n ",yylineno);}
+|Exp MULTI Exp {
+		$$=newAst("Exp",3,$1,$2,$3); 
+		// 操作数类型不匹配或操作数类型与操作符不匹配
+		if(strcmp($1->type,$3->type))
+			printf("Error at Line %d:Type mismatched for operands.\n ",yylineno);}
+|Exp DIV Exp {
+		$$=newAst("Exp",3,$1,$2,$3); 
+		// 操作数类型不匹配或操作数类型与操作符不匹配
+		if(strcmp($1->type,$3->type))
+			printf("Error at Line %d:Type mismatched for operands.\n ",yylineno);}
+|LP Exp RP {$$=newAst("Exp",3,$1,$2,$3); }
+|MINUS Exp {$$=newAst("Exp",2,$1,$2); }
+|NOT Exp {$$=newAst("Exp",2,$1,$2); }
+|ID LP Args RP {
+		$$=newAst("Exp",4,$1,$2,$3,$4); 
+		if(!findfunc($1) && (findvar($1)||findarray($1))) // 对普通变量使用“(...)”或“()”（函数调用）操作符
+			printf("Error at Line %d:'%s' is not a function.\n ",yylineno,$1->content);
+		else if(!findfunc($1)) // 函数在调用时未经定义
+			printf("Error at Line %d:Undefined function %s\n ",yylineno,$1->content);
+		else if(checkrtype($1,$3))// 函数实参和形参类型不一致
+			printf("Error at Line %d:Function parameter type error.\n ",yylineno);
+		else{ /*empty*/ }}
+|ID LP RP {
+		$$=newAst("Exp",3,$1,$2,$3); 
+		if(!findfunc($1) && (findvar($1)||findarray($1))) // 对普通变量使用“(...)”或“()”（函数调用）操作符
+			printf("Error at Line %d:'%s' is not a function.\n ",yylineno,$1->content);
+		else if(!findfunc($1)) 	// 函数在调用时未经定义
+			printf("Error at Line %d:Undefined function %s\n ",yylineno,$1->content);
+		else {}
+	}
+|Exp LB Exp RB {$$=newAst("Exp",4,$1,$2,$3,$4); }
+|Exp DOT ID {$$=newAst("Exp",3,$1,$2,$3); }
+|ID {
+		$$=newAst("Exp",1,$1); 
+		if(!findvar($1)&&!findarray($1)) // 变量在使用时未经定义
+			printf("Error at Line %d:undefined variable or array.%s\n",yylineno,$1->content);
+		else 
+			$$->type=typevar($1);}
+|INT {$$=newAst("Exp",1,$1); $$->tag=3;$$->type="int";}
+|BREAK {$$=newAst("Exp",1,$1);}
 ;
 
 Args:
-    /* empty */             { /* empty */ }
-|   _Args                   {printf("\n\n"); }
-;
-
-_Args:
-    T_Int T_Identifier      { printf("\targ %s", $2); }
-|   _Args ',' T_Int T_Identifier
-                            { printf(", %s", $4); }
-;
-
-VarDecls:
-    /* empty */             { /* empty */ }
-|   VarDecls VarDecl ';'    { printf("\n\n"); }
-;
-
-VarDecl:
-    T_Int T_Identifier      { printf("\tvar %s", $2); }
-|   VarDecl ',' T_Identifier
-                            { printf(", %s", $3); }
-;
-
-Statements:
-    /* empty */             { /* empty */ }
-|   Statements Statement              { /* empty */ }
-;
-
-Statement:
-    AssignStatement              { printf("finish assign statement\n"); }
-|   PrintStatement               { printf("finish print statement\n");}
-|   CallStatement                { printf("finish call statement\n");}
-|   ReturnStatement              { printf("finish return statement\n"); }
-|   IfStatement                  { printf("finish if statement\n"); }
-|   WhileStatement               { printf("finish while statement\n"); }
-|   BreakStatement               { printf("finish break statement\n"); }
-|   ContinueStatement            { printf("finish continue statement\n"); }
-;
-
-AssignStatement:
-    T_Identifier '=' Expr ';'
-                            { printf("\tpop %s\n\n", $1); }
-;
-
-PrintStatement:
-    T_Print '(' T_StringConstant PActuals ')' ';'
-                            { printf("\tprint %s\n\n", $3); }
-;
-
-PActuals:
-    /* empty */             { /* empty */ }
-|   PActuals ',' Expr       { /* empty */ }
-;
-
-CallStatement:
-    CallExpr ';'            { printf("\tpop\n\n"); }
-;
-
-CallExpr:
-    T_Identifier '(' Actuals ')'
-                            { printf("\t$%s\n", $1); }
-;
-
-Actuals:
-    /* empty */             { /* empty */ }
-|   Expr PActuals           { /* empty */ }
-;
-
-ReturnStatement:
-    T_Return Expr ';'       { printf("\tret ~\n\n"); }
-|   T_Return ';'            { printf("\tret\n\n"); }
-;
-
-IfStatement:
-    If TestExpr StatementsBlock
-                            { /*empty*/ }
-|   If TestExpr StatementsBlock Else StatementsBlock
-                            { /*empty*/ }
-;
-
-TestExpr:
-    '(' Expr ')'            { printf("finish testExpression\n");}
-;
-
-StatementsBlock:
-    '{' Statements '}'           { printf("finish statementsBlock\n"); }
-;
-
-If:
-    T_If            { /*empty*/}
-;
-
-
-Else:
-    T_Else          { /* empty */ }
-;
-
-WhileStatement:
-    While TestExpr StatementsBlock
-                    { printf("finish whileStatement\n");}
-;
-
-While:
-    T_While         { /*empty*/}
-;
-
-BreakStatement:
-    T_Break ';'     { /*empty*/}
-;
-
-ContinueStatement:
-    T_Continue ';'  { /*empty*/}
-;
-
-Expr:
-    Expr '+' Expr           { printf("\tadd\n"); }
-|   Expr '-' Expr           { printf("\tsub\n"); }
-|   Expr '*' Expr           { printf("\tmul\n"); }
-|   Expr '/' Expr           { printf("\tdiv\n"); }
-|   Expr '%' Expr           { printf("\tmod\n"); }
-|   Expr '>' Expr           { printf("\tcmpgt\n"); }
-|   Expr '<' Expr           { printf("\tcmplt\n"); }
-|   Expr T_Ge Expr          { printf("\tcmpge\n"); }
-|   Expr T_Le Expr          { printf("\tcmple\n"); }
-|   Expr T_Eq Expr          { printf("\tcmpeq\n"); }
-|   Expr T_Ne Expr          { printf("\tcmpne\n"); }
-|   Expr T_Or Expr          { printf("\tor\n"); }
-|   Expr T_And Expr         { printf("\tand\n"); }
-|   '-' Expr %prec '!'      { printf("\tneg\n"); }
-|   '!' Expr                { printf("\tnot\n"); }
-|   T_IntConstant           { printf("\tpush %s\n", $1); }
-|   T_Identifier            { printf("\tpush %s\n", $1); }
-|   ReadInt                 { /* empty */ }
-|   CallExpr                { /* empty */ }
-|   '(' Expr ')'            { /* empty */ }
-;
-
-ReadInt:
-    T_ReadInt '(' T_StringConstant ')'
-                            { printf("\treadint %s\n", $3); }
+Exp COMMA Args {$$=newAst("Args",3,$1,$2,$3);}
+|Exp {$$=newAst("Args",1,$1);}
 ;
 
 %%
 
-int main() {
-    return yyparse();
-}
---
+++ syntax_tree.c
@@ -0,0 +1,514 @@
+#include "syntax_tree.h"
+
+// name: 当前创建结点名称
+// num: > 0 表示该节点不是终结符，存在num个子节点；= 0 为终结符结点；= -1 为空结点
+Ast newAst(char *name, int num, ...){
+    // 生成父节点
+    tnode father = (tnode)malloc(sizeof(struct treeNode));
+    // 添加子节点
+    tnode temp = (tnode)malloc(sizeof(struct treeNode));
+    if (!father)
+    {
+        yyerror("create treenode error");
+        exit(0);
+    }
+    father->name = name;
+
+    // 参数列表，详见 stdarg.h 用法
+    va_list list;
+    // 初始化参数列表
+    va_start(list, num);
+
+    if (num > 0) { // 表示当前节点不是终结符号，还有子节点
+        father->ncld = num;
+        // 第一个孩子节点
+        temp = va_arg(list, tnode);
+        father->cld[0] = temp;
+        setChildTag(temp);
+        // 父节点行号为第一个孩子节点的行号
+        father->line = temp->line;
+
+        if (num == 1) {
+            //父节点的语义值等于左孩子的语义值
+            father->content = temp->content;
+            father->tag = temp->tag;
+        } else {
+            for (int i = 1; i < num; i++) {
+                temp = va_arg(list, tnode);
+                (father->cld)[i] = temp;
+                setChildTag(temp);
+            }
+        }
+    } else { //表示当前节点是终结符（叶节点）或者空的语法单元（空单元为-1）
+        father->ncld = 0;
+        father->line = va_arg(list, int);
+
+        if (!strcmp(name, "INT")){ // name == "INT"
+            father->type = "int";
+            father->value = atoi(yytext);
+        } else {  // 存储词法单元语义值 
+            char *str;
+            str = (char *)malloc(sizeof(char) * 40);
+            strcpy(str, yytext);
+            father->content = str;
+        }
+    }
+    nodeList[nodeNum ++] = father;
+
+    return father;
+}
+
+// 前序遍历
+void preOrderAst(Ast ast, int level){
+    if (ast == NULL) return ;
+    else {
+        for (int i = 0; i < level; i ++)
+            printf("  "); // 层级缩进
+        
+        if (ast -> line != -1) {
+            printf("%s", ast -> name); // token类型
+
+            if ((!strcmp(ast->name, "ID")) || (!strcmp(ast->name, "TYPE")))
+                printf(": %s", ast->content);
+            else if (!strcmp(ast->name, "INT"))
+                printf(": %d", (int)ast->value);
+            else // 非叶节点打印行号
+                printf("(%d)", ast->line);
+            
+
+            printf("\n");
+            for (int i = 0; i < ast -> ncld; i ++)
+                preOrderAst((ast->cld)[i], level + 1);
+        }
+    }
+}
+
+void yyerror(char *msg) {
+    hasFault = 1;
+    fprintf(stderr, "Error type B at Line %d: %s before %s\n", yylineno, msg, yytext);
+}
+
+// 标记结点为子节点
+void setChildTag(tnode node) {
+    for (int i = 0; i < nodeNum; i++){
+        if (nodeList[i] == node)
+            nodeIsChild[i] = 1;
+    }
+}
+
+// // 先序遍历分析
+// void analysis(Ast ast){
+//     if (ast == NULL) return ;
+//     else {
+//         for (int i = 0; i < ast->ncld; ++i)
+//         {
+//             analysis((ast->cld)[i]);
+//         }
+//     }
+// }
+
+// 建立变量符号
+void newvar(int num, ...) {
+    va_list valist;
+    va_start(valist, num);
+
+    var *res = (var *)malloc(sizeof(var));
+    tnode temp = (tnode)malloc(sizeof(tnode));
+
+    if (inStruc && LCnum){ // 是结构体域中且{和}不能抵消
+        res->inStruc = 1; // 当前是结构体域
+        res->strucNum = strucNum; //标记是第几个结构体
+    } else {
+        res->inStruc = 0; // 不是结构体域
+        res->strucNum = 0;
+    }
+
+    // 变量声明 e.g. int i
+    temp = va_arg(valist, tnode);
+    res-> type = temp->content;
+    temp = va_arg(valist, tnode);
+    res-> name = temp->content;
+
+    vartail->next = res;
+    vartail = res;
+}
+
+// 查找变量, 检查是否存在变量重复定义
+int findvar(tnode val) {
+    var *temp = (var *)malloc(sizeof(var *));
+    temp = varhead -> next;
+    while (temp != NULL) {
+        if (!strcmp(temp -> name, val -> content)){ //找到同名
+            if (inStruc && LCnum) { // 当前变量在是结构体域内
+                if (temp -> inStruc && temp -> strucNum == strucNum){ // 同一结构体中域名重复
+                    printf("Error at Line %d: Variable redefinition.\n", yylineno);
+                    return 1;
+                }
+            } else { // 当前变量是全局变量
+                if (!temp->inStruc) { // 变量与变量重名，即重复定义
+                    printf("Error at Line %d: Variable redefinition.\n", yylineno);
+                    return 1;
+                }
+            }
+        }
+        temp = temp->next;
+    }
+    return 0;
+}
+
+// 得到变量类型
+char* typevar(tnode val){
+    var *temp = (var *)malloc(sizeof(var *));
+    temp = varhead->next;
+    while (temp != NULL){
+        if (!strcmp(temp->name, val->content))
+            return temp->type; //返回变量类型
+
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+// 赋值号左边只能出现标识符、数组、结构体的变量, 1正0误
+// <ID>、<Exp LB Exp RB>、<Exp DOT ID>
+int checkleft(tnode val){
+    if (val->ncld == 1 && !strcmp((val->cld)[0]->name, "ID"))
+        return 1;
+    else if (val->ncld == 4 && !strcmp((val->cld)[0]->name, "Exp") && !strcmp((val->cld)[1]->name, "LB") && !strcmp((val->cld)[2]->name, "Exp") && !strcmp((val->cld)[3]->name, "RB"))
+        return 1;
+    else if (val->ncld == 3 && !strcmp((val->cld)[0]->name, "Exp") && !strcmp((val->cld)[1]->name, "DOT") && !strcmp((val->cld)[2]->name, "ID"))
+        return 1;
+    else
+        return 0;
+}
+
+// 创建函数符号
+void newfunc(int num, ...) {
+    va_list valist;
+    va_start(valist, num);
+
+    tnode temp = (tnode)malloc(sizeof(tnode));
+
+    switch (num){
+    case 1:
+        if (inStruc && LCnum){ // 是结构体的域
+            functail->inStruc = 1;
+            functail->strucNum = strucNum;
+        } else {
+            functail->inStruc = 0;
+            functail->strucNum = 0;
+        }
+        //设置函数返回值类型
+        temp = va_arg(valist, tnode);
+        functail-> rtype = temp->content;
+        functail-> type = temp->type;
+        for (int i = 0; i < rnum; i++){
+            if (rtype[i] == NULL || strcmp(rtype[i], functail->rtype))
+                printf("Error at Line %d:Func return type error.\n", yylineno);
+        }
+        functail ->tag = 1; //标志为已定义
+        func *new = (func *)malloc(sizeof(func));
+        functail->next = new; //尾指针指向下一个空结点
+        functail = new;
+        break;
+    case 2:
+        //记录函数名
+        temp = va_arg(valist, tnode);
+        functail -> name = temp -> content;
+        //设置函数声明时的参数
+        temp = va_arg(valist, tnode);
+        functail->va_num = 0;
+        getdetype(temp);
+        break;
+    default:
+        break;
+    }
+}
+
+//定义的参数
+void getdetype(tnode val)
+{
+    int i;
+    if (val != NULL)
+    {
+        if (!strcmp(val->name, "ParamDec"))
+        {
+            functail->va_type[functail->va_num] = val->cld[0]->content;
+            functail->va_num++;
+            return;
+        }
+        for (i = 0; i < val->ncld; ++i)
+        {
+            getdetype((val->cld)[i]);
+        }
+    }
+    else
+        return;
+}
+
+//实际的参数
+void getretype(tnode val)
+{
+    int i;
+    if (val != NULL)
+    {
+        if (!strcmp(val->name, "Exp"))
+        {
+            va_type[va_num] = val->type;
+            va_num++;
+            return;
+        }
+        for (i = 0; i < val->ncld; ++i)
+        {
+            getretype((val->cld)[i]);
+        }
+    }
+    else
+        return;
+}
+
+//函数实际返回值类型
+void getrtype(tnode val)
+{
+    rtype[rnum] = val->type;
+    rnum++;
+}
+
+//检查形参与实参是否一致,没有错误返回0
+int checkrtype(tnode ID, tnode Args)
+{
+    int i;
+    va_num = 0;
+    getretype(Args);
+    func *temp = (func *)malloc(sizeof(func *));
+    temp = funchead->next;
+    while (temp != NULL && temp->name != NULL && temp->tag == 1)
+    {
+        if (!strcmp(temp->name, ID->content))
+            break;
+        temp = temp->next;
+    }
+    if (va_num != temp->va_num)
+        return 1;
+    for (i = 0; i < temp->va_num; i++)
+    {
+        if (temp->va_type[i] == NULL || va_type[i] == NULL || strcmp(temp->va_type[i], va_type[i]) != 0)
+            return 1;
+    }
+    return 0;
+}
+
+// 函数是否已经定义
+int findfunc(tnode val)
+{
+    func *temp = (func *)malloc(sizeof(func *));
+    temp = funchead->next;
+    while (temp != NULL && temp->name != NULL && temp->tag == 1)
+    {
+        if (!strcmp(temp->name, val->content))
+        {
+            if (inStruc && LCnum) // 当前变量是结构体域
+            {
+                if (!temp->inStruc)
+                {
+                    // 结构体域与变量重名
+                    printf("Error type 9 at Line %d:Struct Field and Variable use the same name.\n", yylineno);
+                }
+                else if (temp->inStruc && temp->strucNum != strucNum)
+                {
+                    // 不同结构体中的域重名
+                    printf("Error type 10 at Line %d:Struct Fields use the same name.\n", yylineno);
+                }
+                else
+                {
+                    // 同一结构体中域名重复
+                    return 1;
+                }
+            }
+            else // 当前变量是全局变量
+            {
+                if (temp->inStruc)
+                {
+                    // 变量与结构体域重名
+                    printf("Error type 9 at Line %d:Struct Field and Variable use the same name.\n", yylineno);
+                }
+                else
+                {
+                    // 变量与变量重名，即重复定义
+                    return 1;
+                }
+            }
+        }
+        temp = temp->next;
+    }
+    return 0;
+}
+
+// 函数类型
+char *typefunc(tnode val)
+{
+    func *temp = (func *)malloc(sizeof(func *));
+    temp = funchead->next;
+    while (temp != NULL)
+    {
+        if (!strcmp(temp->name, val->content))
+            return temp->type; //返回函数类型
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+// 形参个数
+int numfunc(tnode val)
+{
+    func *temp = (func *)malloc(sizeof(func *));
+    temp = funchead->next;
+    while (temp != NULL) {
+        if (!strcmp(temp->name, val->content))
+            return temp->va_num; //返回形参个数
+        temp = temp->next;
+    }
+}
+
+// 创建数组符号表
+void newarray(int num, ...)
+{
+    va_list valist;
+    va_start(valist, num);
+
+    array *res = (array *)malloc(sizeof(array));
+    tnode temp = (tnode)malloc(sizeof(struct treeNode));
+
+    if (inStruc && LCnum)
+    {
+        // 是结构体的域
+        res->inStruc = 1;
+        res->strucNum = strucNum;
+    }
+    else
+    {
+        res->inStruc = 0;
+        res->strucNum = 0;
+    }
+    // int a[10]
+    temp = va_arg(valist, tnode);
+    res->type = temp->content;
+    temp = va_arg(valist, tnode);
+    res->name = temp->content;
+    arraytail->next = res;
+    arraytail = res;
+}
+
+// 数组是否已经定义
+int findarray(tnode val){
+    array *temp = (array *)malloc(sizeof(array *));
+    temp = arrayhead->next;
+    while (temp != NULL)
+    {
+        if (!strcmp(temp->name, val->content))
+            return 1;
+        temp = temp->next;
+    }
+    return 0;
+}
+
+// 数组类型
+char *typearray(tnode val)
+{
+    array *temp = (array *)malloc(sizeof(array *));
+    temp = arrayhead->next;
+    while (temp != NULL)
+    {
+        if (!strcmp(temp->name, val->content))
+            return temp->type; //返回数组类型
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+// 创建结构体符号表
+void newstruc(int num, ...) {
+    va_list valist;
+    va_start(valist, num);
+
+    struc *res = (struc *)malloc(sizeof(struc));
+    tnode temp = (tnode)malloc(sizeof(struct treeNode));
+
+    // struct name{}
+    temp = va_arg(valist, tnode);
+    res->name = temp->content;
+    structail->next = res;
+    structail = res;
+}
+
+// 结构体是否和结构体或变量的名字重复
+int findstruc(tnode val)
+{
+    struc *temp = (struc *)malloc(sizeof(struc *));
+    temp = struchead->next;
+    while (temp != NULL)
+    {
+        if (!strcmp(temp->name, val->content))
+            return 1;
+        temp = temp->next;
+    }
+    if (findvar(val) == 1)
+        return 1;
+    return 0;
+}
+
+// 主函数 扫描文件并且分析
+// bison会自己调用yylex()，所以在main函数中不需要再调用它了
+// bison使用yyparse()进行语法分析，所以需要调用yyparse()和yyrestart()
+int main(int argc, char **argv) {
+    if (argc < 2)
+        return 1;
+    
+    for (int i = 1; i < argc; i++){
+        // 初始化，用于记录结构体
+        inStruc = 0;
+        LCnum = 0;
+        strucNum = 0;
+
+        // 初始化符号表
+        varhead = (var *)malloc(sizeof(var));
+        vartail = varhead;
+        funchead = (func *)malloc(sizeof(func));
+        functail = (func *)malloc(sizeof(func));
+        funchead->next = functail;
+        functail->va_num = 0;
+        arrayhead = (array *)malloc(sizeof(array));
+        arraytail = arrayhead;
+        struchead = (struc *)malloc(sizeof(struc));
+        structail = struchead;
+        rnum = 0;
+
+        // 初始化节点记录列表
+        nodeNum = 0;
+        memset(nodeList, 0, sizeof(tnode) * 5000);
+        memset(nodeIsChild, 0, sizeof(int) * 5000);
+        hasFault = 0;
+
+        FILE *f = fopen(argv[i], "r");
+        if (!f){
+            perror(argv[i]);
+            return 1;
+        }
+
+        yyrestart(f);
+        yyparse();
+        fclose(f);
+
+        //存在语法错误，下一个文件
+        if (hasFault)
+            continue;
+  
+        for (int j = 0; j < nodeNum; j++){
+            if (nodeIsChild[j] != 1) { // 遍历所有非子节点的节点
+                preOrderAst(nodeList[j], 0);
+            }
+        }
+    }
+    return 0;
+}
+
