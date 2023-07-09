24a18044c3be67a5d7873234143c6a402c81aece
Trea19 <1242734867@qq.com>
2023-07-09 10:58:36 +0800

    main modify output

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ extern FILE *in;
 FILE *out;
 extern AST_Node *root;
 extern int error_flag;
+int out_is_file;
 
 int main(int argc, char **argv) {
     if (argc > 1){
@@ -20,20 +21,26 @@ int main(int argc, char **argv) {
     }
     if (argc > 2){
     	if (!(out = fopen(argv[2], "w"))) {
-		perror(argv[2]);
-		return 1;
-	}
+			perror(argv[2]);
+			return 1;
+		}
+		out_is_file = 1;
     } else {
+		out_is_file = 0;
     	out = stdout;
     }
 	
     root = malloc(sizeof(AST_Node));
 	yyparse();
+	// if no lexical / parser error
     if (error_flag == 0){
         // print_AST(root, 0);
-		// semantics_analysis(root);
-		if (error_flag)
+		semantics_analysis(root);
+		// if semantics error exists
+		if (error_flag){
 			print_error_list();
+		}
+		// if no semantics error
 		else {
 			ir_generate(root);
 			if (OUTPUT_OPTION == 0) {
@@ -45,6 +52,9 @@ int main(int argc, char **argv) {
 			}
 		}
     }
-	fclose(out);
+	if (out_is_file){
+		fclose(out);
+	}
+	
 	return 0;
 }
